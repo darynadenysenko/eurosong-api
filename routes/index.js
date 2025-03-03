5ecfbbc92f11c14b3801234808aeddb64cda9318
Daryna Denysenko <darinadenisenko2204@gmail.com>
2025-03-03 09:21:56 +0100

    Added nodemon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Eurosong' });
+  res.render('index', { title: 'Eurosong1' });
 });
 
 module.exports = router;
